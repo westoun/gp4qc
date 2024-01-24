f748dfd2fe495cc835bfd378050fd56d12df777f
westoun <christoph.werner.stein@gmail.com>
2024-01-24 11:45:04 +0100

    Add validity constraints

--- fitness/validity_checks.py
+++ fitness/validity_checks.py
@@ -8,6 +8,12 @@ def has_exactly_1_oracle(chromosome: List[Gate]) -> bool:
     oracle_gates = [gate for gate in chromosome if issubclass(gate.__class__, Oracle)]
     return len(oracle_gates) == 1
 
+def uses_oracle(chromosome: List[Gate]) -> bool:
+    for gate in chromosome:
+        if issubclass(gate.__class__, Oracle):
+            return True 
+        
+    return False 
 
 def has_exactly_1_input(chromosome: List[Gate]) -> bool:
     input_gates = [
@@ -22,6 +28,13 @@ def has_input_at_first_position(chromosome: List[Gate]) -> bool:
 def uses_hadamard(chromosome: List[Gate]) -> bool:
     for gate in chromosome:
         if type(gate) in [Hadamard, HadamardLayer]:
+            return True 
+
+    return False 
+
+def uses_hadamard_layer(chromosome: List[Gate]) -> bool:
+    for gate in chromosome:
+        if type(gate) == HadamardLayer:
             return True 
 
     return False 
