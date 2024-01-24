741d2c0bef6fb0717d750430db45babb6ca62157
westoun <christoph.werner.stein@gmail.com>
2024-01-24 11:03:22 +0100

    Implement hadamard constraint

--- fitness/validity_checks.py
+++ fitness/validity_checks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from gates import Gate, Oracle, InputEncoding
+from gates import Gate, Oracle, InputEncoding, HadamardLayer, Hadamard
 from typing import List
 
 
@@ -18,3 +18,10 @@ def has_exactly_1_input(chromosome: List[Gate]) -> bool:
 
 def has_input_at_first_position(chromosome: List[Gate]) -> bool:
     return issubclass(chromosome[0].__class__, InputEncoding)
+
+def uses_hadamard(chromosome: List[Gate]) -> bool:
+    for gate in chromosome:
+        if type(gate) in [Hadamard, HadamardLayer]:
+            return True 
+
+    return False 
