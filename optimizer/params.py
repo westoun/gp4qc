0959fec606d6c10ad43e4f7bc8b6e3be9617dbbc
westoun <christoph.werner.stein@gmail.com>
2024-01-25 16:14:38 +0100

    Add comment

--- optimizer/params.py
+++ optimizer/params.py
@@ -2,11 +2,16 @@
 
 from dataclasses import dataclass, field
 
+
 @dataclass
 class OptimizerParams:
     qubit_num: int = 2
     measurement_qubit_num: int = 2
+    # As explained by the scipy documentation: "When tol is specified, the
+    # selected minimization algorithm sets some relevant solver-specific
+    # tolerance(s) equal to tol."
     tolerance: float = 0
     max_iter: int = 10
+
 
 default_params = OptimizerParams()
