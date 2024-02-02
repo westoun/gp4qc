6a6509b408aeec2282222a7ba0a8c79c610abac2
westoun <christoph.werner.stein@gmail.com>
2024-02-02 11:30:33 +0100

    Change variable name

--- gates/gate_set.py
+++ gates/gate_set.py
@@ -8,14 +8,16 @@ from .oracle import Oracle
 
 
 class GateSet:
+    gates: List[Gate] = []
+
     def __init__(self, gates: List[Type[Gate]], qubit_num: int) -> None:
-        self._gates = gates
+        self.gates = gates
         self._qubit_num = qubit_num
 
     def random_gate(self) -> Gate:
         """Selects a gate type at random and initializes it."""
 
-        GateType = choice(self._gates)
+        GateType = choice(self.gates)
         gate = GateType(qubit_num=self._qubit_num)
 
         return gate
