919fa5e7209235ff9254b9f79ef681075f272aad
westoun <christoph.werner.stein@gmail.com>
2024-02-02 11:31:25 +0100

    Implement gate correlation computation

--- examples/probability_distribution.py
+++ examples/probability_distribution.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from functools import partial
 import matplotlib.pyplot as plt
+import numpy as np
 from typing import List
+import warnings
 
 from gates import (
     Gate,
@@ -37,24 +40,87 @@ from optimizer import (
 from fitness.validity_checks import uses_parametrized_gates, uses_hadamard
 
 
+def compute_gate_correlation_every(
+    ga: GA,
+    population: List[List[Gate]],
+    fitness_values: List[float],
+    generation: int,
+    n=5,
+) -> None:
+    if generation % n != 0:
+        return
+
+    fitness_values = [
+        value % 100 for value in fitness_values
+    ]  # Remove punishment terms
+
+    gate_indicators = {}
+    for gate in ga.gate_set.gates:
+        gate_indicators[gate.name] = []
+
+    for chromosome in population:
+        encountered_gates = [gate.name for gate in chromosome]
+        encountered_gates = set(encountered_gates)
+
+        for key in gate_indicators:
+            if key in encountered_gates:
+                gate_indicators[key].append(1)
+            else:
+                gate_indicators[key].append(0)
+
+    print(f"\nCorrelations after generation {generation}:")
+    for key in gate_indicators:
+        if np.std(gate_indicators[key]) == 0:
+            if gate_indicators[key][0] == 0:
+                print(f"\t{key}: NAN (not present in any chromosome)")
+            else:
+                print(f"\t{key}: NAN (present in every chromosome)")
+        else:
+            correlation = np.corrcoef(gate_indicators[key], fitness_values)[0, 1]
+            print(f"\t{key}: {correlation}")
+
+
+compute_gate_correlation = partial(compute_gate_correlation_every, n=1)
+compute_gate_correlation_every_5 = partial(compute_gate_correlation_every, n=5)
+
+
 def run_create_probability_distribution():
     gate_set: GateSet = GateSet(
-        gates=[Hadamard, X, Y, Z, CRX, CRY, CRZ, Phase, Swap, Identity], qubit_num=2
+        gates=[
+            Hadamard,
+            X,
+            Y,
+            Z,
+            CX,
+            CY,
+            RX,
+            RZ,
+            RY,
+            CZ,
+            CRX,
+            CRY,
+            CRZ,
+            Phase,
+            Swap,
+            Identity,
+        ],
+        qubit_num=2,
     )
-    target_distributions: List[List[float]] = [[1 / 6, 1 / 6, 1 / 6, 0.5]]
+    target_distributions: List[List[float]] = [[0, 0.25, 0.25, 0.5]]
     ga_params = GAParams(
         population_size=200,
-        generations=50,
+        generations=20,
         crossover_prob=0.4,
         swap_gate_mutation_prob=0.2,
         swap_order_mutation_prob=0.1,
         chromosome_length=5,
         log_average_fitness_at=1,
-        fitness_threshold=0.08,
+        fitness_threshold=0.001,
+        fitness_threshold_at=3,
     )
 
     fitness_params = FitnessParams(
-        validity_checks=[uses_parametrized_gates, uses_hadamard]
+        # validity_checks=[uses_parametrized_gates, uses_hadamard]
     )
     fitness: Fitness = Jensensshannon(params=fitness_params)
 
@@ -66,6 +132,9 @@ def run_create_probability_distribution():
     )
 
     genetic_algorithm = GA(gate_set, fitness, optimizer, params=ga_params)
+
+    genetic_algorithm.on_after_generation(compute_gate_correlation_every_5)
+    genetic_algorithm.on_completion(compute_gate_correlation)
     genetic_algorithm.run()
 
     TOP_N = 3
