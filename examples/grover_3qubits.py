97d1c170ea834ad5b629563a8028134a0dd04623
westoun <christoph.werner.stein@gmail.com>
2024-02-26 15:31:33 +0100

    Implement correlation computation

--- examples/grover_3qubits.py
+++ examples/grover_3qubits.py
@@ -25,7 +25,7 @@ from gates import (
     HadamardLayer,
     XLayer,
     YLayer,
-    ZLayer
+    ZLayer,
 )
 from ga import GA, GAParams
 from fitness import Fitness, Jensensshannon, FitnessParams, SpectorFitness
@@ -67,6 +67,69 @@ def state_to_distribution(target_state: List[int]) -> List[float]:
     return distribution.tolist()
 
 
+def compute_bigram_correlations(
+    ga: GA,
+    population: List[List[Gate]],
+    fitness_values: List[float],
+    generation: int,
+) -> None:
+    if generation % 3 != 0:
+        return
+
+    population = [ga.toolbox.clone(ind) for ind in population]
+
+    fitness_values = [
+        value % 100 for value in fitness_values
+    ]  # Remove punishment terms
+
+    bigrams = {}
+    for gate1 in ga.gate_set.gates:
+        if type(gate1) == Identity:
+            continue
+
+        for gate2 in ga.gate_set.gates:
+            if type(gate2) == Identity:
+                continue
+
+            bigram = gate1.name + "_" + gate2.name
+            bigrams[bigram] = []
+
+    for chromosome in population:
+        chromosome_bigrams = set()
+
+        for i in range(len(chromosome) - 1):
+            gate = chromosome[i]
+            successor_gate = chromosome[i + 1]
+
+            if type(gate) == Identity or type(successor_gate) == Identity:
+                continue
+
+            bigram = gate.name + "_" + successor_gate.name
+            chromosome_bigrams.add(bigram)
+
+        for bigram in bigrams:
+            if bigram in chromosome_bigrams:
+                bigrams[bigram].append(1)
+            else:
+                bigrams[bigram].append(0)
+
+    print(f"\nRelevant correlations after generation {generation}:")
+    for bigram in bigrams:
+        if np.std(bigrams[bigram]) == 0:
+            if bigrams[bigram][0] == 0:
+                # not present in any chromosome
+                pass 
+            else:
+                print(f"\t{bigram}: NAN (present in every chromosome)")
+        elif sum(bigrams[bigram]) < ga.params.population_size * 0.1:
+            continue 
+        else:
+            correlation = np.corrcoef(bigrams[bigram], fitness_values)[0, 1]
+
+            if correlation > 0.2:
+                print(f"\t{bigram}: {correlation}")
+
+
 def run_grover():
     target_states = [
         [0, 0, 0],
@@ -103,14 +166,14 @@ def run_grover():
             HadamardLayer,
             XLayer,
             YLayer,
-            ZLayer
+            ZLayer,
         ],
         qubit_num=3,
     )
 
     ga_params = GAParams(
         population_size=500,
-        generations=10,
+        generations=20,
         crossover_prob=0.4,
         swap_gate_mutation_prob=0.1,
         swap_order_mutation_prob=0.1,
@@ -132,7 +195,10 @@ def run_grover():
     average_fitness_values = []
 
     def log_fitness_callback(
-        ga: GA, population: List[List[Gate]], fitness_values: List[float], generation: int
+        ga: GA,
+        population: List[List[Gate]],
+        fitness_values: List[float],
+        generation: int,
     ) -> None:
         fitness_values = [
             value % 100 for value in fitness_values
@@ -140,13 +206,14 @@ def run_grover():
         average_fitness = mean(fitness_values)
         average_fitness_values.append(average_fitness)
 
+    genetic_algorithm.on_after_generation(compute_bigram_correlations)
     genetic_algorithm.on_after_generation(log_fitness_callback)
     genetic_algorithm.run()
 
     plt.plot(average_fitness_values)
     plt.xlabel("generation")
     plt.ylabel("mean fitness")
-    plt.savefig("mean_fitness.png")
+    plt.savefig("tmp/mean_fitness.png")
     plt.show()
 
     TOP_N = 3
