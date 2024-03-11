087a58cd903bb9ae159c1dd2cd0861a3773bf605
westoun <christoph.werner.stein@gmail.com>
2024-03-11 13:14:09 +0100

    Rename experiments folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from examples import run_bell_state_2qubits, \
+from experiments import run_bell_state_2qubits, \
     run_bell_state_3qubits, run_half_adder, \
         run_deutsch, run_create_probability_distribution, \
         run_grover
